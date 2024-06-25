187a2e06a04003d1346b75a5027901824df56b74
Pat Nakajima <patnakajima@gmail.com>
2024-06-25 04:14:42 +0000

    wip

--- config/nvim/lua/plugins/copilot.lua
+++ config/nvim/lua/plugins/copilot.lua
@@ -4,13 +4,10 @@ return {
 		cmd = "Copilot",
 		event = "InsertEnter",
 		config = function()
-			require('copilot').setup()
+			require('copilot').setup({
+				suggestion = { enabled = false },
+			  panel = { enabled = false },
+			})
 		end
-	},
-	{
-		"zbirenbaum/copilot-cmp",
-		config = function()
-			require("copilot_cmp").setup()
-		end,
 	},
 }
