345eaff275254eb3ff10470a4dd4ac8a4a1a1256
Pat Nakajima <patnakajima@gmail.com>
2024-07-29 06:09:29 +0000

    ok i think that did it

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -86,19 +86,12 @@ return {
 			callback = function(args)
 				-- Once we've attached, configure the keybindings
 				local wk = require('which-key')
-				wk.register({
-					K = { vim.lsp.buf.hover, "LSP hover info" },
-					gd = { vim.lsp.buf.definition, "LSP go to definition" },
-					gD = { vim.lsp.buf.declaration, "LSP go to declaration" },
-					gi = { vim.lsp.buf.implementation, "LSP go to implementation" },
-					gr = { vim.lsp.buf.references, "LSP list references" },
-					gs = { vim.lsp.buf.signature_help, "LSP signature help" },
-					gn = { vim.lsp.buf.rename, "LSP rename" },
-					["[g"] = { vim.diagnostic.goto_prev, "Go to previous diagnostic" },
-					["g]"] = { vim.diagnostic.goto_next, "Go to next diagnostic" },
-				}, {
+				wk.add({
 					mode = 'n',
 					silent = true,
+					{ "gd", vim.lsp.buf.definition, desc = "LSP go to definition" },
+					{ "[g", vim.diagnostic.goto_prev, desc = "Go to previous diagnostic" },
+					{ "g]", vim.diagnostic.goto_next, desc = "Go to next diagnostic" },
 				})
 			end,
 		})
