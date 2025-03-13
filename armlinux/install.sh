ab31faff1fe5adf0891d9a5cb2c32b631349d84c
Pat Nakajima <patnakajima@gmail.com>
2025-03-13 14:35:49 -0700

    i'm a nix guy now

--- armlinux/install.sh
+++ armlinux/install.sh
@@ -1,26 +1,12 @@
 # There's no brew on arm linux
-
-sudo apt install -y \
-	jq \
-	wget \
-	zsh-autosuggestions \
-	zsh-syntax-highlighting \
-	neovim \
-	lua5.4 \
-	build-essential \
-	ripgrep 
-
-# that's enough sudo for now
-sudo -k
-
-echo "installing atuin..."
-curl --proto '=https' --tlsv1.2 -sSf https://setup.atuin.sh | sh
-
-echo "installing starship..."
-curl -sS https://starship.rs/install.sh | sh
-
-echo "install lua-language-server..."
-mkdir lua-language-server
-cd lua-language-server
-wget https://github.com/LuaLS/lua-language-server/releases/download/3.13.8/lua-language-server-3.13.8-linux-arm64.tar.gz	
-tar -xf lua-language-server-3.13.8-linux-arm64.tar.gz
+nix profile install \
+  nixpkgs#jq \
+  nixpkgs#wget \
+  nixpkgs#neovim \
+  nixpkgs#ripgrep \
+  nixpkgs#zsh \
+  nixpkgs#zsh-autosuggestions \
+  nixpkgs#zsh-syntax-highlighting \
+  nixpkgs#atuin \
+  nixpkgs#starship \
+  nixpkgs#lua-language-server
