5f1ef4ab1ebca448ed0abe81a31b2a2177cffe64
Pat Nakajima <patnakajima@gmail.com>
2025-05-28 08:46:34 -0700

    Create minimal.sh

---
+++ apt/minimal.sh
@@ -0,0 +1,13 @@
+#! /usr/bin/bash
+
+sudo apt update
+sudo apt upgrade -y
+sudo apt install ripgrep zsh gh build-essential -y
+
+# build neovim because the debian package is too old
+git clone https://github.com/neovim/neovim --depth=1
+cd neovim && make CMAKE_BUILD_TYPE=RelWithDebInfo
+cd build && cpack -G DEB && sudo dpkg -i neovim/build/nvim-linux-x86_64.deb
+
+curl --proto '=https' --tlsv1.2 -LsSf https://setup.atuin.sh | sh
+sudo chsh $USER -s "$(which zsh)"
