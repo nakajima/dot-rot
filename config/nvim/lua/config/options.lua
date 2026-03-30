ac29b02a53eda76615f6ecf0958e81c7522c3074
Pat Nakajima <patnakajima@gmail.com>
2026-03-30 11:46:34 -0700

    cleanup

--- config/nvim/lua/config/options.lua
+++ config/nvim/lua/config/options.lua
@@ -2,8 +2,18 @@ local opt = vim.opt
 
 opt.termguicolors = true
 
+vim.o.autoread = true
+
+local group = vim.api.nvim_create_augroup("checktime", { clear = true })
+
+vim.api.nvim_create_autocmd({ "FocusGained", "BufEnter", "CursorHold", "CursorHoldI" }, {
+	group = group,
+	pattern = "*",
+	command = "silent! checktime",
+})
+
 -- Everyone says to do this lol
-opt.backspace = {"start", "indent", "eol"}
+opt.backspace = { "start", "indent", "eol" }
 
 opt.autoindent = true
 opt.expandtab = false
@@ -18,14 +28,13 @@ vim.cmd("nnoremap <cmd>Fs <cmd>!swiftformat .<cr>")
 
 vim.diagnostic.config({ virtual_text = true })
 
-local state = vim.fn.stdpath("state")           -- ~/.local/state/nvim
+local state     = vim.fn.stdpath("state") -- ~/.local/state/nvim
 local backupdir = state .. "/backup//"
 local undodir   = state .. "/undo//"
 vim.fn.mkdir(backupdir, "p")
-vim.fn.mkdir(undodir,   "p")
-vim.opt.backup       = true      -- keep a pre-save copy: file~
-vim.opt.writebackup  = true      -- temp file while writing (safer)
-vim.opt.undofile     = true      -- persistent undo on disk
-vim.opt.backupdir    = backupdir
-vim.opt.undodir      = undodir
-
+vim.fn.mkdir(undodir, "p")
+vim.opt.backup      = true  -- keep a pre-save copy: file~
+vim.opt.writebackup = true  -- temp file while writing (safer)
+vim.opt.undofile    = true  -- persistent undo on disk
+vim.opt.backupdir   = backupdir
+vim.opt.undodir     = undodir
